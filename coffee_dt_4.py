3f39a3554a9d485a357aab59b73d42ed817c56ce
maryam <msajedinia@fbk.eu>
2025-02-12 16:54:33 +0100

    Update: new constraint added

--- coffee_dt_4.py
+++ coffee_dt_4.py
@@ -1,4 +1,4 @@
-# NOTE: version v0.3.0, we are adding another context variable to the model.
+# NOTE: version v0.3.0, we are adding another constraint to the model. The model is now considering the capacity of the food service.       
 from dt_model import UniformCategoricalContextVariable, PresenceVariable, Constraint, Ensemble, Model, Index
 import numpy as np
 from sympy import Symbol
@@ -18,25 +18,28 @@ food_customers = PresenceVariable(
     cvs= [],
 )
 
-# Capacity Index
-capacity = Index('capacity', 50)
+# Capacity Index can be defined as a 
+seat_capacity = Index('seat_capacity', 50)
+food_capacity = Index('food_capacity', 100)
 
 # NOTE: We are going to change the usage factors to 0.5 for both drink and food customers
-U_drink_customers = Index('drink service usage factor', 0.5)
-U_food_customers = Index('food service usage factor', 0.5)
+U_drink_customers = Index('drink service usage factor', 0.3)
+U_food_customers = Index('food service usage factor', 0.7)
 
 # Constraints
-C_drink_customers = Constraint(usage=drink_customers*U_drink_customers + food_customers*U_food_customers, capacity=capacity)
+seat_constraint = Constraint(usage=drink_customers*U_drink_customers + food_customers*U_food_customers, capacity=seat_capacity)
+food_constraint = Constraint(usage=food_customers*U_food_customers, capacity=food_capacity)
+
 
 # Define the model
 model = Model(
     'coffee_shop',
     cvs=[CV_weekday],
     pvs=[drink_customers, food_customers],
-    indexes=[capacity, U_drink_customers, U_food_customers],
-    constraints=[C_drink_customers],
-    capacities=[capacity]
-)
+    indexes=[seat_capacity, U_drink_customers, U_food_customers],
+    constraints=[seat_constraint, food_constraint],
+    capacities=[seat_capacity, food_capacity]
+)   
 
 ensemble = Ensemble(model, {CV_weekday: days}, cv_ensemble_size=7)
 
